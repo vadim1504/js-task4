c0de938f084aa44a3147bd90ccd65cca2cd3bfc9
vadim1504 <vadi-96@mail.ru>
2016-12-10 03:32:08 +0300

    .

--- script/script.js
+++ script/script.js
@@ -1,10 +1,21 @@
 var SPA = (function () {
 
-    let activ;
+    var activ;
+    var documentsData;
+    var fragments;
+    var Document = {
+        name: false,
+        fragments: []
+    };
 
     var initDocument = function () {
         createPopup1();
         createPopup2();
+        initMenu();
+        getDocumentsList();
+    };
+
+    var getDocumentsList = function () {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'http://localhost:3000/docs.svc/getDocumentsList', true);
         xhr.send();
@@ -14,86 +25,79 @@ var SPA = (function () {
                 alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
                 return;
             }
-            var documentsData = JSON.parse(xhr.responseText);
+            documentsData = JSON.parse(xhr.responseText);
             var nav = document.getElementById("nav");
-            for(i in documentsData) {
+            for(var i in documentsData) {
                 var currentDocument = documentsData[i];
                 var d2 = document.createElement("div");
                 d2.className="document";
                 d2.innerText = currentDocument.name;
-                listFragments(currentDocument,d2);
+                d2.setAttribute("id",i);
+                d2.onclick = listFragments;
                 nav.appendChild(d2);
             }
         };
-        initMenu();
     };
 
-    var listFragments = function (currentDocument,d2) {
-        let d1 = d2;
-        d1.onclick = function (event) {
-            var target = event.target;
-            if (target.className == 'document') {
-                if (d1.childNodes.length > 1) {
-                    while (d1.childNodes.length > 1) {
-                        d1.removeChild(d1.lastChild);
-                    }
-                    removeBody();
-                } else {
-                    bodyDocument(currentDocument);
-                    if(activ!=undefined){
-                    while (activ.childNodes.length > 1) {
-                        activ.removeChild(activ.lastChild);
-                    }
-                    }
-                    activ = d1;
-                    for (let i in currentDocument.fragments) {
-                        let currentParagraph = currentDocument.fragments[i];
-                        let d = document.createElement("div");
-                        d.className = "fragNav";
-                        d.innerText = currentParagraph.name;
-                        d.onclick = function (event) {
-                            var target = event.target;
-                            if (target.className == 'fragNav') {
-                                let q = document.getElementsByClassName("paragraph");
-                                if(q[currentParagraph.id].getAttribute("activ")=="0") {
-                                    getParagraph(currentDocument.id, currentParagraph.id, q[currentParagraph.id]);
-                                }
-                                else{
-                                    q[currentParagraph.id].setAttribute("activ","0");
-                                    q[currentParagraph.id].innerText = "Paragraph: "+currentParagraph.name+"\n\n";
-                                }
-                            }
-                        };
-                        d1.appendChild(d);
-                    }
+    function listFragments(event) {
+        var d1 = event.target;
+        var id = d1.getAttribute("id");
+        var currentDocument = documentsData[id];
+        if (d1.childNodes.length > 1) {
+            while (d1.childNodes.length > 1) {
+                d1.removeChild(d1.lastChild);
+            }
+            removeBody();
+        } else {
+            if(activ!=undefined){
+                while (activ.childNodes.length > 1) {
+                    activ.removeChild(activ.lastChild);
                 }
             }
+            activ = d1;
+            fragments = currentDocument.fragments;
+            for (var i in fragments) {
+                var currentParagraph = fragments[i];
+                var d = document.createElement("div");
+                d.className = "fragNav";
+                d.innerText = currentParagraph.name;
+                d.setAttribute("id",i);
+                d.onclick = activParagraph;
+                d1.appendChild(d);
+            }
+            bodyDocument(currentDocument);
         }
-    };
 
-    var bodyDocument = function (currentDocument) {
-        let doc = currentDocument;
+    }
+
+    function activParagraph(event) {
+        event.stopPropagation();
+        var el = event.target;
+        var id = el.getAttribute("id");
+        var currentParagraph = fragments[id];
+        var idPared = activ.getAttribute("id");
+        var currentDocument = documentsData[idPared];
+        var q = document.getElementsByClassName("paragraph");
+        if(q[currentParagraph.id].getAttribute("activ")=="0") {
+            getParagraph(currentDocument.id, currentParagraph.id, q[currentParagraph.id]);
+        }
+        else{
+            q[currentParagraph.id].setAttribute("activ","0");
+            q[currentParagraph.id].innerText = "Paragraph: "+currentParagraph.name+"\n\n";
+        }
+    }
+
+    var bodyDocument = function () {
         removeBody();
-        let b = document.getElementById("bodyDocument");
-        for(i in currentDocument.fragments) {
-            let frag = currentDocument.fragments[i];
-            let b2 = document.createElement("div");
+        var b = document.getElementById("bodyDocument");
+        for(var i in fragments) {
+            var frag = fragments[i];
+            var b2 = document.createElement("div");
             b2.className="paragraph";
             b2.innerText = "Paragraph: "+frag.name+"\n\n";
             b2.setAttribute("activ","0");
             b2.setAttribute("id",i);
-            b2.onclick = function (event) {
-                var target = event.target;
-                if (target.tagName != 'div'){
-                    if(b2.getAttribute("activ")=="0") {
-                        getParagraph(doc.id, frag.id, b2);
-                    }
-                    else{
-                        b2.setAttribute("activ","0");
-                        b2.innerText = "Paragraph: "+frag.name+"\n\n";
-                    }
-                }
-            };
+            b2.onclick = activParagraph;
             b.appendChild(b2);
         }
     };
@@ -113,14 +117,12 @@ var SPA = (function () {
         };
     };
 
-
     var removeBody = function () {
         var b = document.getElementById("bodyDocument");
         while (b.childNodes.length>1){
             b.removeChild(b.lastChild);
         }
     };
-
 
     var createPopup1 = function () {
         var divPopup =document.createElement("div");
@@ -157,81 +159,88 @@ var SPA = (function () {
         document.body.appendChild(divPopup);
     };
 
-
     var initMenu = function () {
-        document.getElementById("new_document").onclick = function () {
-            document.getElementById("popup1").style.visibility = "visible";
-            document.getElementById("createDoc").onclick = function () {
-                Document.name = document.getElementById("nameNewDoc").value;
-                var nav = document.getElementById("nav");
-                var newDoc = document.createElement("div");
-                newDoc.className="document";
-                newDoc.innerText = document.getElementById("nameNewDoc").value;
-                nav.appendChild(newDoc);
-                document.getElementById("new_paragraph").style.opacity="1";
-                document.getElementById("save").style.opacity="1";
-                document.getElementById("popup1").style.visibility = "hidden";
-            };
-            document.getElementById("back").onclick = function () {
-                document.getElementById("popup1").style.visibility = "hidden";
-            };
-        };
+        document.getElementById("new_document").onclick = newDocClick;
+        document.getElementById("new_paragraph").onclick = newParagraphClick;
+        document.getElementById("save").onclick = save;
+    };
 
-        document.getElementById("new_paragraph").onclick = function () {
-            if(Document.name){
-                document.getElementById("popup2").style.visibility = "visible";
-                document.getElementById("createParagraph").onclick = function () {
-                    var name = document.getElementById("nameParagraph").value;
-                    var content = document.getElementById("context").value;
-                    Document.fragments.push({
-                        name: name,
-                        content: content
-                    });
-                    let b = document.getElementById("bodyDocument");
-                    let b2 = document.createElement("div");
-                    b2.className = "paragraph";
-                    b2.innerText = "Paragraph: "+name+"\n\n"+content;
-                    b.appendChild(b2);
-                    document.getElementById("popup2").style.visibility = "hidden";
-                };
-                document.getElementById("backParagraph").onclick = function () {
-                    document.getElementById("popup2").style.visibility = "hidden";
-                };
-            }else{
-                alert("Сначало нужно создать документ!!!");
+    function newDocClick() {
+        document.getElementById("popup1").style.visibility = "visible";
+        document.getElementById("createDoc").onclick = createDoc;
+        document.getElementById("back").onclick = function () {
+            document.getElementById("popup1").style.visibility = "hidden";
+        };
+    }
+
+    function createDoc() {
+        Document.name = document.getElementById("nameNewDoc").value;
+        var nav = document.getElementById("nav");
+        var newDoc = document.createElement("div");
+        newDoc.className="document";
+        newDoc.innerText = document.getElementById("nameNewDoc").value;
+        removeBody();
+        if(activ!=undefined){
+            while (activ.childNodes.length > 1) {
+                activ.removeChild(activ.lastChild);
             }
-        };
+        }
+        activ = newDoc;
+        nav.appendChild(newDoc);
+        document.getElementById("new_paragraph").style.opacity="1";
+        document.getElementById("save").style.opacity="1";
+        document.getElementById("popup1").style.visibility = "hidden";
+    }
 
-        document.getElementById("save").onclick = function () {
-            if(Document.name){
-                var xhr = new XMLHttpRequest();
-                var params = 'document=' + JSON.stringify(Document);
-                xhr.open('POST', 'http://localhost:3000/docs.svc/saveDocument', true);
-                xhr.send(params);
-                xhr.onreadystatechange = function () {
-                    if (this.readyState != 4) return;
-                    if (this.status != 200) {
-                        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-                        return;
-                    }
-                    alert('Документ сохранён');
+    function newParagraphClick() {
+        if(Document.name){
+            document.getElementById("popup2").style.visibility = "visible";
+            document.getElementById("createParagraph").onclick = createParagraph;
+            document.getElementById("backParagraph").onclick = function () {
+                document.getElementById("popup2").style.visibility = "hidden";
+            };
+        }else{
+            alert("Сначало нужно создать документ!!!");
+        }
+    }
+
+    function createParagraph() {
+        var name = document.getElementById("nameParagraph").value;
+        var content = document.getElementById("context").value;
+        Document.fragments.push({
+            name: name,
+            content: content
+        });
+        var b = document.getElementById("bodyDocument");
+        var b2 = document.createElement("div");
+        b2.className = "paragraph";
+        b2.innerText = "Paragraph: "+name+"\n\n"+content;
+        b.appendChild(b2);
+        document.getElementById("popup2").style.visibility = "hidden";
+    }
+
+    function save() {
+        if(Document.name){
+            var xhr = new XMLHttpRequest();
+            var params = 'document=' + JSON.stringify(Document);
+            xhr.open('POST', 'http://localhost:3000/docs.svc/saveDocument', true);
+            xhr.send(params);
+            xhr.onreadystatechange = function () {
+                if (this.readyState != 4) return;
+                if (this.status != 200) {
+                    alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+                    return;
                 }
-            }else{
-                alert("Сначало нужно создать документ!!!");
+                alert('Документ сохранён');
             }
-        };
-
-    };
-
-    var Document = {
-        name: false,
-        fragments: []
-    };
+        }else{
+            alert("Сначало нужно создать документ!!!");
+        }
+    }
 
     return{
         initDocument : initDocument
     }
-
 })();
 
  window.onload = SPA.initDocument;
